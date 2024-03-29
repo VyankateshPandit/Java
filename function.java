95b33e33d77298ff53acafd99debeda1286a54c6
vyankatesh pandit <ompandit1511@gmail.com>
2024-03-29 19:34:10 +0530

    new function file

---
+++ function.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class function{
+
+
+public static void p_m_name(int n){
+     
+ 
+      int fact =1;
+
+
+       if(n < 0)
+       {
+              System.out.println("Error!");
+              return;
+       }
+
+        else{
+            for(int i = 1 ; i<=n ; i++){
+                fact= fact*i; 
+       }
+
+        System.out.println("Factorial is :"+ fact);
+     
+        }
+    
+}
+
+
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+           int n =sc.nextInt();
+    
+         p_m_name(n);
+
+sc.close();
+}
+}
